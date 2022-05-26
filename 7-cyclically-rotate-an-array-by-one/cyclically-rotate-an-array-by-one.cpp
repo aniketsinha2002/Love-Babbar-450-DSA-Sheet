2d1dd3dd57f3de156ea91873bdbed98130a6a665
Aniket Sinha <97850511+aniketsinha2002@users.noreply.github.com>
2022-05-26 12:29:58 +0530

    Create cyclically-rotate-an-array-by-one.cpp

---
+++ 7-cyclically-rotate-an-array-by-one/cyclically-rotate-an-array-by-one.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <stdio.h>
+
+void rotate(int arr[], int n);
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+        return 0;
+}
+// } Driver Code Ends
+
+
+void rotate(int arr[], int n)
+{   int temp[n];
+    temp[0] = arr[n-1];
+    
+    for(int i=0;i<n-1;i++){
+        
+        temp[i+1]=arr[i];
+       
+    }
+    
+    for(int i=0;i<n;i++){
+        
+        arr[i]=temp[i];
+       
+    }
+    
+     
+    
+}
